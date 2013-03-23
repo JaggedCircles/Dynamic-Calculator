99197c915011cce48227bf0f3e06e6b675534f43
Liam Westby <lw322@nau.edu>
2013-03-23 12:06:44 -0700

    Initial Commit.

---
+++ src/dynamicNetworking/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package dynamicNetworking.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/dynamicNetworking/calculator/operationsServer/Operation.java
@@ -0,0 +1,5 @@
+package dynamicNetworking.calculator.operationsServer;
+
+public class Operation {
+
+}
